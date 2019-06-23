0be938a17a896346df125b797d00951d658b4f74
ramazan.karagoz <ramazan.karagoz.official@gmail.com>
2019-06-23 12:31:14 +0300

    crud

--- src/main/java/com/ramazan/spring/jsf/example/controller/KitapController.java
+++ src/main/java/com/ramazan/spring/jsf/example/controller/KitapController.java
@@ -1,10 +1,14 @@
 package com.ramazan.spring.jsf.example.controller;
 
+import com.ramazan.spring.jsf.example.entity.Kitap;
 import com.ramazan.spring.jsf.example.service.KitapService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Controller
@@ -12,9 +16,33 @@ import org.springframework.stereotype.Controller;
 public class KitapController {
 
     private final KitapService kitapService;
+    private Kitap kitap=new Kitap();
+    private Kitap selectedKitap;
+    private List<Kitap> kitapList=new ArrayList<>();
 
     @Autowired
     public KitapController(KitapService kitapService) {
         this.kitapService = kitapService;
     }
+
+    public void onKaydet(){
+
+        kitapService.kaydet(kitap);
+    }
+
+    public void onUpdate(){
+        if (selectedKitap!=null)
+        kitapService.update(selectedKitap);
+    }
+
+    public void onDelete(){
+        if (selectedKitap!=null)
+            kitapService.sil(selectedKitap);
+    }
+
+    public void onListele(){
+        kitapList=kitapService.findAll();
+    }
+
+
 }
